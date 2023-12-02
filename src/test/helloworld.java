22bff3a7c760ef769c2a7b6ec02f13dffdc92965
tootaemi <n4mtkk@gmail.com>
2023-12-02 19:51:35 +0900

    Test1

---
+++ src/test/helloworld.java
@@ -0,0 +1,19 @@
+package test;
+
+//文字を出力するプログラム
+//複数行出力するプログラム
+//演算を行うプログラム
+
+//hello world
+//私の名前は〇〇です
+//よろしくお願いします
+
+public class helloworld {
+    public static void main(String args[]){    //Javaプログラムを実行するために必要な形式的なもの
+	
+	    System.out.println("hello world");
+	    System.out.println("私の名前は遠田江美です");
+	    System.out.println("よろしくお願いします");
+    	System.out.println(3*5);  //3*5
+	}
+}
---
+++ src/test/kadai1.java
@@ -0,0 +1,10 @@
+package test;
+
+public class kadai1 {
+	    public static void main(String args[]){    		
+		    System.out.println("私の名前はえみです");
+	    	System.out.println(1+1);
+	    	System.out.println(4/2);
+		}
+	}
+
