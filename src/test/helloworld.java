8192bc3e0ef0d21be8f7a29f0406c90d56ace32d
tootaemi <n4mtkk@gmail.com>
2023-12-09 22:50:26 +0900

    testt1

--- src/test/helloworld.java
+++ src/test/helloworld.java
@@ -12,8 +12,8 @@ public class helloworld {
     public static void main(String args[]){    //Javaプログラムを実行するために必要な形式的なもの
 	
 	    System.out.println("hello world");
-	    System.out.println("私の名前は遠田江美です");
-	    System.out.println("よろしくお願いします");
+	    System.out.println("私の名前は遠田です");
+	    System.out.println("よろしくお願いします。");
     	System.out.println(3*5);  //3*5
 	}
 }
