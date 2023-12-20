618d338d92c1f9031bacff9fc33441eea87ded4f
tootaemi <n4mtkk@gmail.com>
2023-12-20 22:59:31 +0900

    shuusei-ok

--- src/test/kadai1.java
+++ src/test/kadai1.java
@@ -2,7 +2,7 @@ package test;
 
 public class kadai1 {
 	    public static void main(String args[]){    		
-		    System.out.println("私の名前はえみです");
+		    System.out.println("私の名前は遠田です");
 	    	System.out.println(1+1);
 	    	System.out.println(4/2);
 		}
