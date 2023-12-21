7c9c8bea535b424cec9647c6e3766ab7edbebd44
tootaemi <n4mtkk@gmail.com>
2023-12-21 23:02:57 +0900

    算術演算子-練習

--- src/test/helloworld.java
+++ src/test/helloworld.java
@@ -10,10 +10,34 @@ package test;
 
 public class helloworld {
     public static void main(String args[]){    //Javaプログラムを実行するために必要な形式的なもの
-	
-	    System.out.println("hello world");
-	    System.out.println("私の名前は遠田江美です");
-	    System.out.println("よろしくお願いします。");
-    	System.out.println(3*5);  //3*5
+//	    System.out.println("hello world");
+//	    System.out.println("私の名前は遠田江美です");
+//	    System.out.println("よろしくお願いします。");
+//    	System.out.println(3*5);  //3*5
+    	
+    	int year;
+    	
+    	year = 2020;
+    	
+    	//+,-,*,/,%
+    	//5わる2を正しく出力する
+    	
+    	int next_year;
+    	
+    	next_year = year +1;
+    	year = next_year -2;
+    	
+    	year = next_year *2;
+    	year = year /2;
+    	
+    	int mod;
+    	
+    	mod = 4%3;
+    	
+    	double num;
+    	
+    	num = 5.0/2.0;
+    	
+    	System.out.println(mod);
 	}
 }
