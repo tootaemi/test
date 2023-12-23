0394248332cc9554cd884c58a3c84ba3b85347d6
tootaemi <n4mtkk@gmail.com>
2023-12-24 00:35:59 +0900

    復習2

--- src/test/helloworld.java
+++ src/test/helloworld.java
@@ -1,4 +1,4 @@
-package test;
+//package test;
 
 //文字を出力するプログラム
 //複数行出力するプログラム
@@ -8,13 +8,25 @@ package test;
 //私の名前は〇〇です
 //よろしくお願いします
 
-public class Main {
-    public static void main(String[] args) {
+//public class Main {
+//    public static void main(String[] args) {
         // ここにプログラムの処理を記述します
 //        System.out.println("Hello, world!"); // 例として文字列を出力してみます
 //    }
 //}
 
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
+
+
+
+
+//
 //public class helloworld {　　//helloworld じゃなくてmainじゃないと表示できない！！
 //    public static void main(String args[]){    //Javaプログラムを実行するために必要な形式的なもの
 //	    System.out.println("hello world");
@@ -37,14 +49,14 @@ public class Main {
 //    	year = next_year *2;
 //    	year = year /2;
     	
-    	int mod;
-    	
-    	mod = 4%3;
+//    	int mod;
+//    	
+//    	mod = 4%3;
 ////    	
 ////    	double num;
 ////    	
 ////    	num = 5.0/2.0;
 //    	
-    	System.out.println(mod);
-	}
-}
+//    	System.out.println(mod);
+//	}
+//}
--- src/test/kadai1.java
+++ src/test/kadai1.java
@@ -1,8 +1,8 @@
 package test;
 
-public class kadai1 {
+public class Main {
 	    public static void main(String args[]){    		
-		    System.out.println("私の名前は遠田です");
+		    System.out.println("私の名前は遠田です,");
 	    	System.out.println(1+1);
 	    	System.out.println(4/2);
 		}
