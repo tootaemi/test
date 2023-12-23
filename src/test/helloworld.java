770ab327ef6b34fe3ac5e0d0e7af489c3c08aee2
tootaemi <n4mtkk@gmail.com>
2023-12-23 23:51:42 +0900

    復習

--- src/test/helloworld.java
+++ src/test/helloworld.java
@@ -8,37 +8,43 @@ package test;
 //私の名前は〇〇です
 //よろしくお願いします
 
-public class helloworld {
-    public static void main(String args[]){    //Javaプログラムを実行するために必要な形式的なもの
+public class Main {
+    public static void main(String[] args) {
+        // ここにプログラムの処理を記述します
+//        System.out.println("Hello, world!"); // 例として文字列を出力してみます
+//    }
+//}
+
+//public class helloworld {　　//helloworld じゃなくてmainじゃないと表示できない！！
+//    public static void main(String args[]){    //Javaプログラムを実行するために必要な形式的なもの
 //	    System.out.println("hello world");
 //	    System.out.println("私の名前は遠田江美です");
 //	    System.out.println("よろしくお願いします。");
 //    	System.out.println(3*5);  //3*5
-    	
-    	int year;
-    	
-    	year = 2020;
+//    	
+//    	int year;
+//    	
+//    	year = 2020;
     	
     	//+,-,*,/,%
     	//5わる2を正しく出力する
     	
-    	int next_year;
-    	
-    	next_year = year +1;
-    	year = next_year -2;
-    	
-    	year = next_year *2;
-    	year = year /2;
+//    	int next_year;
+//    	
+//    	next_year = year +1;
+//    	year = next_year -2;
+//    	
+//    	year = next_year *2;
+//    	year = year /2;
     	
     	int mod;
     	
     	mod = 4%3;
-    	
-    	double num;
-    	
-    	num = 5.0/2.0;
-    	
+////    	
+////    	double num;
+////    	
+////    	num = 5.0/2.0;
+//    	
     	System.out.println(mod);
-    	
 	}
 }
