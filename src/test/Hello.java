9550ae2c1887cb6e6d8672f2998805d7c088f306
tootaemi <n4mtkk@gmail.com>
2024-01-06 00:18:23 +0900

    配列

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -2,7 +2,80 @@ package test;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
-}
+        //System.out.println("Hello, world!");
+        
+        
+//        ・配列の仕組み
+//        配列
+//        ・同一種類の複数データを並び順に格納するデータ構造。
+//        int型   int[]score
+//                 [0] [1] [2] [3] [4]        
+//        ・一つの配列で複数のデータを扱える。
+//        
+//        ・配列の宣言　データ型[]配列名　= new データ型[要素数];
+//        int[]score = new int[5]       //注意点、配列は0から始まる
+        
+        
+        
+//        int [] score = new int[6];
+//        
+//        score[0] = 92;     //japanese
+//        score[1] = 98;     //math
+//        score[2] = 88;     //geo
+//        score[3] = 89;     //science
+//        score[4] = 78;     //english
+//        score[5] = 82;     //history   //ここ後で追加しても下のiは自動的に変わるから書き換えなくて済む。
+        
+//        System.out.println(score[0]);
+//        System.out.println(score.length);      
+//                                               
+//        
+//        int sum=0;         //合計の変数宣言
+//        int average=0;      //平均の変数宣言
+//        
+//        for(int i=0; i< score.length; i++) {     //score.lengthで全て足すことができる。
+//        	 //score.lengthは要素数が5,iが5から始まると, //iは5より小さい間30行目のfor文を実行する
+//        	//0から4まで実行するfor文なので0から4まで足せる。
+//        	sum = sum + score[i];
+//        }
+//        average = sum/score.length;
+//        System.out.println(sum);     //445
+//        System.out.println(average); //89
 
+        
+        int [] score1 = {92,94,99,98,97};
+        int [] score2;
+        
+        score2 = score1;
+        score2[0] = 88;
+        
+        System.out.println(score1[0]);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+    
