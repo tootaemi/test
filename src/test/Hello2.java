8837f261250934129d23559a5dcaf52248c32d1b
tootaemi <n4mtkk@gmail.com>
2024-01-06 11:59:04 +0900

    引数の利用

--- src/test/Hello2.java
+++ src/test/Hello2.java
@@ -1,10 +1,23 @@
 package test;
 
+
+//引数の利用の続き
+
 public class Hello2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello2");
+		//hello("太郎");    ///helloメソッ呼び出す。
+		//hello("次郎");
 
+		hello("太郎","三郎");    ///helloメソッ呼び出す。
+		hello("次郎","四郎");
+		
+	}
+	
+	public static void hello(String name1,String name2) {
+		
+		System.out.println("Helli World!");   //"Hello World!"を出力する。
+		System.out.println("こんばんは" + name1 + "さん" + name2 + "さん");
 	}
 
 }
