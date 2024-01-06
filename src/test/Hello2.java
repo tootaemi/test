200983ffa44822dae7e374b8d6d099ec25a77b54
tootaemi <n4mtkk@gmail.com>
2024-01-06 12:04:11 +0900

    引数のおさらい

--- src/test/Hello2.java
+++ src/test/Hello2.java
@@ -6,18 +6,25 @@ package test;
 public class Hello2 {
 
 	public static void main(String[] args) {
-		//hello("太郎");    ///helloメソッ呼び出す。
-		//hello("次郎");
+		hello("太郎");    ///helloメソッ呼び出す。
+		hello("次郎");
 
-		hello("太郎","三郎");    ///helloメソッ呼び出す。
-		hello("次郎","四郎");
+//		hello("太郎","三郎");    ///helloメソッ呼び出す。
+//		hello("次郎","四郎");
 		
 	}
 	
-	public static void hello(String name1,String name2) {
-		
+	public static void hello(String name) {
 		System.out.println("Helli World!");   //"Hello World!"を出力する。
-		System.out.println("こんばんは" + name1 + "さん" + name2 + "さん");
+		System.out.println("こんばんは" + name + "さん");
 	}
+		
+	
+	
+//	public static void hello(String name1,String name2) {
+//		
+//		System.out.println("Helli World!");   //"Hello World!"を出力する。
+//		System.out.println("こんばんは" + name1 + "さん" + name2 + "さん");
+//	}
 
 }
