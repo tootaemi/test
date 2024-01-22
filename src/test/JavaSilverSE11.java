0d01a3197e8e83a267381c6fb53cd41928c70082
tootaemi <n4mtkk@gmail.com>
2024-01-22 23:34:27 +0900

    第1章.簡単なjavaプログラムの作成.途中

--- src/test/JavaSilverSE11.java
+++ src/test/JavaSilverSE11.java
@@ -1,8 +1,85 @@
 package test;
 
-public class JavaSilverSE11 {
-	public static void main(String[] args) {
-		System.out.println("JavaSilverSE11");
+		public class JavaSilverSE11 {
+			public static void main(String[] args) {
+				System.out.println("JavaSilverSE11");
+				
+				
+
+			
+		
+		
+//		第1章
+//		簡単なjavaプログラムの作成
+//		・パッケージ
+//		・クラスのインポート
+//		・mainメソッド
+//		・javaコマンド
+//		
+//		
+//		1.パッケージの説明として正しいもの
+//		→A.C.D
+				//生異空間を提供する
+				//アクセス制限を提供する
+				//クラスの分類を可能にする
+		
+		//2.パッケージ宣言が正しく記述されてるコード
+		//B
+		
+		//3.インポート宣言しなくても自動的にインポートされるもの
+		//A,C	
+		//java.langパッケージに属するクラス
+		//同じパッケージに属するクラス
+				
+		
+		//4.
+		
+		
+//		public class Sample {
+//			protected int num = 10;
+//		}
+//		
+//		package ex4;
+//		
+//		public class SampleImpl extends Sample {
+//			public static void main(String[] args) {
+//				System.out.println(num);
+//
+//				→コンパイルエラーがj発生する。
+	
+
+		
+
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
