749c4c5c1a848598b2d4be0b5a351e3b3153b115
tootaemi <n4mtkk@gmail.com>
2024-01-23 23:28:22 +0900

    第1章.簡単なjavaプログラムの作成..終了

--- src/test/JavaSilverSE11.java
+++ src/test/JavaSilverSE11.java
@@ -23,17 +23,21 @@ package test;
 				//アクセス制限を提供する
 				//クラスの分類を可能にする
 		
-		//2.パッケージ宣言が正しく記述されてるコード
-		//B
+//		2.パッケージ宣言が正しく記述されてるコード
+//		B
+//		→
+//		package aaa;
+//		import java.io.*;
+//		public class Sample {}
 		
+				
 		//3.インポート宣言しなくても自動的にインポートされるもの
 		//A,C	
 		//java.langパッケージに属するクラス
 		//同じパッケージに属するクラス
 				
 		
-		//4.
-		
+		//4.どうなるか
 		
 //		public class Sample {
 //			protected int num = 10;
@@ -47,12 +51,64 @@ package test;
 //
 //				→コンパイルエラーがj発生する。
 	
+//
+//				5.アプリケーションのエントリーポイントとなるメソッドの
+//				条件として正しいもの
+//				
+//				A.B.E
+//				→publicであること
+//				staticであること
+//				引数はString配列型もしくはString型の可変長引数であること
 
-		
+				
+//				6.次のプログラムを以下に示したコマンドで実行した時の結果として正しいものは？
 
-		
-		
-
+//				> java Main java one two
+//				
+//				public class Main {
+//					public static void main(String[] args) {
+//						System.out.println(args[0] + " " + args[1]);
+//					}
+//				}
+//				
+//				→「java one」と表示される。
+//				
+//				構文
+//				java 完全修飾クラス名 [引数　引数　・・・]
+//				
+//						
+//				7.javaのクラスを実行するコマンドとして正しいもの
+//				→
+//				java Test
+//				java Test.java
+////		
+//
+//				8.Sampleクラスを実行した時の結果として正しいもの
+//				
+//				> java Sample a ¥" a¥" "a "b c
+//				
+//				public class SAmple {
+//					public static void main(String[] args) {
+//						System.out.println(args.length);
+//						}
+//					}
+//				
+//				→5が表示される
+//				3つのポイント
+//				・スペースが区切り記号として扱われる。
+//				・スペースを含む文字列はダブルクォーテーションで括ることで1つとして扱われる。
+//				・ダブルクォーテーションそのものは文字として扱わない。
+//				
+//				
+				
+				
+				
+				
+				
+				
+				
+//					}
+//				}
 		
 		
 		
