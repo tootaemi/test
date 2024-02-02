c80270873452936d4afe9efa1f0d2dff0f85e5bd
tootaemi <n4mtkk@gmail.com>
2024-02-02 23:56:24 +0900

    第4章7まで

---
+++ src/test/JavaSilverSE11_0.java
@@ -0,0 +1,140 @@
+package test;
+
+public class JavaSilverSE11_0 {
+	public static void main(String[] args) {
+//		System.out.println("JavaSilverSE11_0");
+
+		
+		
+		//第4章4から
+		
+		//4.
+//		//public class Main {
+//			//public static void main(String[] args) {
+//				for (int i = 1, long j = 2; i < 5; i++) {
+//					System.out.println(i * j);
+//		
+//		⇨　コンパイルエラーになる。
+//		初期化文としてint型とlong型の異なる型の変数を
+//		同時に宣言しているためコンパイルエラーになってしまう。
+//		
+		//！　for文の初期化式では同じ型の変数を複数宣言できる。
+		//異なる型の変数を同時に宣言することはできない、コンパイルエラーになる。
+		
+////		　　　　　　　　　　↓　直すとしたら下記のように書く。
+//					
+//					
+////		public class Main {
+////		    public static void main(String[] args) {
+//		        int j = 2; // jをforループの外で宣言
+//		        for (int i = 1; i < 5; i++) 
+		//System.out.println(i * j);
+		
+		
+////		5.
+//		int a = 1;
+//		for (int b = 2, total = 0; b <= 5; b++) {
+//			total = a * b;
+//		}
+//		System.out.println(total);
+//		
+//		
+//		→　コンパイルエラーになる。
+//		下記のように直す。
+		
+		
+//		int a = 1;
+//		int total = 0; // total を for ループの外で宣言し、初期化
+//		for (int b = 2; b <= 5; b++) {
+//		    total = a * b;
+//		}
+//		System.out.println(total); // ここで total の値を出力できる
+//
+//		コンソールに5が表示される。
+		
+		
+		//6.コンソールに「0」と表示したい。
+//		
+//		for (int i = 0; i == 0; i++) {
+//			System.out.println(i);
+//		}
+//		
+//		!　条件文は繰り返し処理を実行する前に判定される。
+//		　　条件文の結果がtrueなら繰り返し処理を実行、faiseならfor文を抜ける。
+		
+//		7.
+//		for (int i = 0, j = 0; i < 3, j < 5; i++) {
+//			System.out.println(i++);
+//			j += i;
+//		}
+		
+		for (int i = 0, j = 0; i < 3 && j < 5; i++) {
+		    System.out.println(i++);
+		    j += i; // ここでコロンをセミコロンに修正
+		}
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		        }
+		    }
+		
+
+				
+
+
+
+
+
+
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
