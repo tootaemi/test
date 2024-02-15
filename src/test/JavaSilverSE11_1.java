6b099026e96f17105578ac2e9849669a50f3298a
tootaemi <n4mtkk@gmail.com>
2024-02-15 22:50:33 +0900

    第7章〜

--- src/test/JavaSilverSE11_1.java
+++ src/test/JavaSilverSE11_1.java
@@ -2,13 +2,47 @@ package test;
 
 public class JavaSilverSE11_1 {
 	public static void main(String[] args) {
-		
 		System.out.println("Hello, world!");
-		System.out.println("私の名前は遠田江美です");
-		System.out.println("よろしくお願いします。");
-		System.out.println(1+1);  //2
-		System.out.println("6-1");  //6-1
-		System.out.println(3*5);  //3*5	
+		
+//		第7章
+//		クラスの継承、インターフェース、抽象クラス
+//		・クラスの継承
+//		・インターフェース
+//		・抽象クラスと具象クラス
+//		・抽象メソッドの実装
+//		・メソッドのオーバーライド
+//		・ポリモーフィズム
+//		・型の互換性、アップキャスト、ダウンキャスト
+//		・thisの使用
+//		・superの使用
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 }
