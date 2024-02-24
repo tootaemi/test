2ae698a27820804e35058d0d601a46e116601a78
tootaemi <n4mtkk@gmail.com>
2024-02-24 11:30:24 +0900

    大丈夫か。。

--- src/test/Taisaku.java
+++ src/test/Taisaku.java
@@ -2,7 +2,7 @@ package test;
 
 public class Taisaku {
 	public static void main(String[] args) {
-		  System.out.println("終わる?");
+		  System.out.println("終わった");
 		
 		
 		// JavaSilverSE11_1 のコード　ここに移して。
