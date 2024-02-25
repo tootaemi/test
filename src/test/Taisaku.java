ba7bee2b5744c8d14d6c514f94f55a8c85bd5415
tootaemi <n4mtkk@gmail.com>
2024-02-25 15:45:46 +0900

    復習3

--- src/test/Taisaku.java
+++ src/test/Taisaku.java
@@ -3,16 +3,7 @@ package test;
 public class Taisaku {
 	public static void main(String[] args) {
 		  System.out.println("終わった");
-		  
-		
-		
-		// JavaSilverSE11_1 のコード　ここに移して。
-		  //⇨//のコードじゃない
-		  //後で確認する
-		 
-		
-		
-		
+	
 	}
 
 }
