3c4206f28b51de28409b15475bcf24e979d935bc
tootaemi <n4mtkk@gmail.com>
2024-02-25 17:47:28 +0900

    コードの移動

--- src/test/Bronze.java
+++ src/test/Bronze.java
@@ -2,7 +2,7 @@ package test;
 
 public class Bronze {
 	public static void main(String[] args) {
-		System.out.println("こんにちは。");
+		//System.out.println("こんにちは。");
 
 		
 		
--- src/test/Shosinsha1.java
+++ src/test/Shosinsha1.java
@@ -3,6 +3,20 @@ package test;
 public class Shosinsha1 {
 	public static void main(String[] args) {
 		
+		//文字を出力するプログラム
+		//複数行出力するプログラム
+		//演算を行うプログラム
+
+		//hello world
+		//私の名前は〇〇です
+		//よろしくお願いします
+
+		//public class Main {
+		//public static void main(String[] args) {
+		    // ここにプログラムの処理を記述します
+//		    System.out.println("Hello, world!"); // 例として文字列を出力してみます
+		//}
+		//}
 		
 		
 		//条件分岐・制御文
@@ -191,26 +205,102 @@ public class Shosinsha1 {
 		
 		
         // 3-1,九九の計算
+//
+//        
+//        for (int i = 1; i <= 9; i++) {
+//            for (int j = 1; j <= 9; j++) {
+//                int result = i * j;
+//                System.out.print(i + "×" + j + "は" + result + "\tです。");
+//            }
+//            System.out.println(); // 次の段へ移るための改行
+//        }
+//     
+//        //3-2
+//        
+//        for(int i = 1; i <= 100; i++){
+//        if(i % 5 == 0 || i % 3 == 0){
+//        if(i % 15 != 0){
+//        System.out.println(i);
+//        }
+//      }
+//    }    
+//   }
+//} 
 
-        
-        for (int i = 1; i <= 9; i++) {
-            for (int j = 1; j <= 9; j++) {
-                int result = i * j;
-                System.out.print(i + "×" + j + "は" + result + "\tです。");
-            }
-            System.out.println(); // 次の段へ移るための改行
-        }
-     
-        //3-2
-        
-        for(int i = 1; i <= 100; i++){
-        if(i % 5 == 0 || i % 3 == 0){
-        if(i % 15 != 0){
-        System.out.println(i);
-        }
-      }
-    }    
-   }
-} 
 
+
+////System.out.println("study");
+//System.out.println("hello world");
+//System.out.println("Hello Java");	    
+//System.out.println("私の名前は遠田江美です");
+//System.out.println("よろしくお願いします。");
+//System.out.println(3*5);  //3*5
+
+
+//public class study {
+////public class Main {　　//helloworld じゃなくてmainじゃないと表示できない！！
+//public static void main(String args[]){    //Javaプログラムを実行するために必要な形式的なもの
+
+//System.out.println("私の名前は遠田江美です");
+//System.out.println("よろしくお願いします。");
+//System.out.println(3*5);  //3*5
+////
+//int year;
+//int next_year;
+//
+//year = 2020;
+//next_year = year +1;
+//
+//int m = Math.min(year, next_year);   
+//year++;  //year =year +1    
+//year--;
+
+//+,-,*,/,%
+//5わる2を正しく出力する
+
+//int next_year;
+////
+//next_year = year +1;
+//year = next_year -2;
+////
+//year = next_year*2;
+//year = year /2;
+
+//System.out.println(year);
+
+//
+//int mod;
+////
+//mod = 5%3;
+////
+//double num;
+//////
+//num = 5.0/2.0;
+//
+
+//文字列結合演算子
+
+//System.out.println("あなたの名前は？");
+//String input1 = new java.util.Scanner(System.in).nextLine();      //文字列
+//
+//System.out.println("あなたの年齢は？");
+//int input2 = new java.util.Scanner(System.in).nextInt();        //整数
+//
+//System.out.println("こんにちは"+ input2 +"歳の" +input1 +"さん");
+
+//課題2
+//System.out.println(1+1);  //1+1
+//System.out.println(2-2);  //2-2
+//System.out.println(3*3);  //3*3
+//System.out.println(4/4);  //4/4
+//
+//int a = 2;
+//int b = 9;
+//int m = Math.max(a, b); // 二つの値を比較して大きい方を取得する
+////int m = Math.max(2,0,9,1);
+//System.out.println("大きいのは" + m + "です");
+		
+		
+		}
+	}
 
