a45ff6803fe1843a42e93c9ba2851c3bdfe54887
tootaemi <n4mtkk@gmail.com>
2024-03-09 22:31:23 +0900

    9日目、終了

--- src/test/Bronze1.java
+++ src/test/Bronze1.java
@@ -2,7 +2,90 @@ package test;
 
 public class Bronze1 {
 	public static void main(String[] args) {
-		  System.out.println("終");
+//		  System.out.println("終");
+		  
+		  
+	//9日目
+	//クラスの継承
+//	
+//		public class A {
+//			private int num;
+//			public void setNum(int num) {
+//				this.num = num;
+//			}
+//		}
+//		
+//		public class B extends A {
+//			private int num;
+//			public int getNum() {
+//				return num;
+//			}
+//		}
+//		
+//		public class InheritanceTest {
+//			public static void main(String[] args) {
+//				B b = new B();
+//				b.setNum(10);
+//				System.out.println(b.getNum());
+//			}
+//		}
+//		  
+//		  
+//		このJavaのコードサンプルは、継承とカプセル化の概念を使用しています。
+//		クラスAは、`num`という名前のprivate変数を持ち、
+//		この変数の値を設定するpublicメソッド`setNum`を提供します。
+//		クラスBは、クラスAを拡張し、同じ名前の`num`という新しいprivate変数を持っています。
+//		クラスBは、この新しい`num`変数の値を返すpublicメソッド`getNum`を提供します。
+//
+//		`InheritanceTest`クラスの`main`メソッドでは、クラスBの
+//		インスタンスが作成され、`setNum`メソッドを通じて`num`に`10`が設定されます。
+//		しかし、`setNum`メソッドはスーパークラスAの`num`変数に値を設定しますが、
+//		`getNum`メソッドはサブクラスBの`num`変数の値を返します。
+//		サブクラスBの`num`変数は、別に初期化されていないので、
+//		`getNum`メソッドはデフォルト値`0`を出力します。
+//
+//
+//		このコードは、継承とカプセル化を示しています。
+//		`A`クラスは`num`というprivate変数を持ち、この変数の値を設定するための
+//		`setNum`というpublicメソッドがあります。
+//		`B`クラスは`A`クラスを継承し、新たに`num`というprivate変数を持っています。
+//		また、`B`クラスはこの`num`変数の値を取得するための
+//		`getNum`というpublicメソッドを持っています。
+//
+//		`InheritanceTest`クラスの`main`メソッドでは、
+//		`B`クラスのインスタンスが生成され、`setNum`メソッドを使って
+//		`num`に`10`を設定します。しかし、`setNum`は
+//		スーパークラス`A`の`num`に値を設定し、
+//		`getNum`メソッドはサブクラス`B`の`num`の値を返します。
+//		`B`の`num`は別途初期化されていないため、
+//		`getNum`メソッドはデフォルト値の`0`を出力します。
+//		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		  
 	}
 }
