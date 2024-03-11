7171f2bb7318c3071f625972eca0d0c66d9efdb0
tootaemi <n4mtkk@gmail.com>
2024-03-11 23:16:09 +0900

    第2章途中

--- src/test/Selfstudy4.java
+++ src/test/Selfstudy4.java
@@ -2,6 +2,32 @@ package test;
 
 public class Selfstudy4 {
 	public static void main(String[] args) {
-		System.out.println("練習");
+//		System.out.println("練習");
+		
+		//スッキリわかるJava入門　読書中。。。
+		
+		//第1章 完了
+		
+		第2章
+		式と演算子
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
