388b699384b4a61a8537d1852c324856d6e4e9b1
tootaemi <n4mtkk@gmail.com>
2024-03-11 23:02:25 +0900

    練習、エディタにメイン型が含まれない。。

---
+++ src/test/Selfstudy4.java
@@ -0,0 +1,7 @@
+package test;
+
+public class Selfstudy4 {
+	public static void main(String[] args) {
+		System.out.println("練習");
+	}
+}
---
+++ src/test/Selfstudy5.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Selfstudy5 {
+	public static void main(String[] args) {
+		System.out.println("練習!!");
+
+		
+		
+	}
+
+}
