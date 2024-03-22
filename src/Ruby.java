913d691e07ffd4f117d005a5d019c97f893f54fe
tootaemi <n4mtkk@gmail.com>
2024-03-22 23:22:42 +0900

    たのしいRuby、P26まで

--- src/Ruby.java
+++ src/Ruby.java
@@ -1,25 +1,39 @@
-
-<h1>ユーザー一覧</h1>
-<div class="row">
-      <table class='table table-hover table-inverse'>
-        <thead>
-          <tr>
-            <th class="bg-light">会員ID</th>
-            <th class="bg-light">ユーザーネーム</th>
-            <th class="bg-light">メールアドレス</th>
-          </tr>
-        </thead>
-        <% @customers.each do |customer| %>
-        <tr>
-          <td><%= customer.id %></td>
-          <td><%= link_to customer.name, admin_customer_path(customer.id) %></td>
-          <td><%= customer.email %></td>
-        </tr>
-        <% end %>
-      </table>
-    </div>
-    <div class="text-center">
-      <%= paginate @customers %>
-    </div>
+//
+//<h1>ユーザー一覧</h1>
+//<div class="row">
+//      <table class='table table-hover table-inverse'>
+//        <thead>
+//          <tr>
+//            <th class="bg-light">会員ID</th>
+//            <th class="bg-light">ユーザーネーム</th>
+//            <th class="bg-light">メールアドレス</th>
+//          </tr>
+//        </thead>
+//        <% @customers.each do |customer| %>
+//        <tr>
+//          <td><%= customer.id %></td>
+//          <td><%= link_to customer.name, admin_customer_path(customer.id) %></td>
+//          <td><%= customer.email %></td>
+//        </tr>
+//        <% end %>
+//      </table>
+//    </div>
+//    <div class="text-center">
+//      <%= paginate @customers %>
+//    </div>
+    
+    
+  //  上記コードは練習　エラーになってしまう。
+    
+    2024年3月22日（金）
+    たのしいRuby　　
+    
+    第1部
+    Rubyをはじめようp26まで読み終わり
+    
+    
+    
+    
+    
     
     
